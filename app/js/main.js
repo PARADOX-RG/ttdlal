51f9eebb0866369b1f168a061d267723d2311222
PARADOX-RG <vkurtik12@gmail.com>
2021-03-05 11:57:20 +0200

    added contact

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
 
+
+
+
 });
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB_nvbNE0iJ-iEIUQZPFuifA_wTvubJU_I",
+    authDomain: "ttdlad-1.firebaseapp.com",
+    projectId: "ttdlad-1",
+    storageBucket: "ttdlad-1.appspot.com",
+    messagingSenderId: "363543803943",
+    appId: "1:363543803943:web:8322b1d9243eddf510b602",
+    measurementId: "G-XE4FCV38TG"
+};
